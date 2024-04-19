e28e9e34ae13c91ad39296abd91f038fe145437b
vaibhav130901 <vaibhavsandeepnidhi@gmail.com>
2024-04-19 23:11:53 +0530

    Use telegram bot

--- helper.js
+++ helper.js
@@ -1,12 +1,15 @@
+const axios = require("axios");
+
 exports.notifyChange = async (message) => {
-  twilio = require("twilio");
-  const client = new twilio(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-  );
-  client.messages.create({
-    from: process.env.TWILIO_PHONE_NUMBER,
-    to: "+91" + process.env.TARGET_PHONE_NUMBER,
-    body: message,
-  });
+  const encodedParam = encodeURIComponent(message);
+
+  const url = `https://api.telegram.org/bot${process.env.BOT_ID}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${encodedParam}`;
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 };
